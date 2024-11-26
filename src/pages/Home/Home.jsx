58cb2141523017992cbf7c24630a5800fed8ed14
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-26 08:57:53 -0300

    :sparkles: feat: Componente da Página Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,26 @@
+import "../../sass/_Home.scss";
+
+export default function Home() {
+  return (
+    <main className="page home">
+      <div className="container">
+        <div className="text-home">
+          <span>So, you want to travel to</span>
+          <h1>Space</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+
+        <div className="circle-box">
+          <div className="circle">
+            <h2>Explore</h2>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
