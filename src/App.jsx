037aa1abf23306a4120c698b10b18f19f6e833cc
tatyanepgoncalves <tatyane.2324165@aluno.faculdadedescomplica.com.br>
2024-11-25 21:25:08 -0300

    :wrench: chore: Configuração da página

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import "./sass/App.scss";
+import "./sass/index.scss";
+
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+
+    </div>
+  )
+}
